d32db6c223af45234d63d614936a53b2854f99c9
martin <martin@sqquid.com>
2022-01-10 21:03:40 -0300

    added extra basic ast functions

---
+++ prototype_ast.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+#include "expr_ast.hpp"
+
+class PrototypeAST {
+  std::string Name;
+  std::vector<std::string> Args;
+
+public:
+  PrototypeAST(const std::string &name, std::vector<std::string> Args)
+    : Name(name), Args(std::move(Args)) {}
+
+  const std::string &getName() const { return Name; }
+};
---
+++ prototype_ast.hpp
@@ -0,0 +1,4 @@
+class PrototypeAST {
+public:
+  PrototypeAST() {}
+};
